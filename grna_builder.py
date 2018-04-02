5098eb4a9a1fd39feacfad7090c6b6b38785a2d3
CJackson <christopher@cajackson.org>
2018-04-02 18:03:43 -0400

    Changed multicore to work better for small genomes. User can provide genomic locations.

--- grna_builder.py
+++ grna_builder.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
 
 from lib.bowtie2 import bowtie_make_index
-from lib.grna_scorer import find_grna
+from lib.grna_scorer import GetCRISPRs
 
 import argparse
 import tempfile
 import shutil
+import multiprocessing
 
 from Bio import SeqIO
 from Bio.Alphabet import generic_dna
@@ -24,17 +25,25 @@ def grna_build_args():
                     type=int)
     ap.add_argument("--cpu", dest="cpu", help="NUMBER of CPU cores to use", metavar="NUMBER", default=1,
                     type=int)
+    ap.add_argument("--location", dest="loc", help="FILE containing the query gene locations", metavar="FILE",
+                    default=None)
+
 
     args = ap.parse_args()
     with open(args.query, mode="rU") as query_fh:
         queries = list(SeqIO.parse(query_fh, format=args.query_type, alphabet=generic_dna))
-        grna_builder(queries, args.genome, genome_type=args.genome_type, outfile_path=args.out, out_num=args.num,
-                     cores=args.cpu)
+        grna_builder(queries, args.genome, genome_type=args.genome_type, loc_path=args.loc, outfile_path=args.out,
+                     out_num=args.num, cores=args.cpu)
 
 
-def grna_builder(queries, genome_path, genome_type="fasta", outfile_path=None, out_num=1, cores=1):
+def grna_builder(queries, genome_path, genome_type="fasta", loc_path= None, outfile_path=None, out_num=1, cores=1):
     if outfile_path is not None:
         out_fh = open(outfile_path, mode="w")
+
+    if loc_path is not None:
+        locations = _parse_locations(loc_path)
+    else:
+        locations = None
 
     try:
         if genome_type != "bowtie":
@@ -43,31 +52,51 @@ def grna_builder(queries, genome_path, genome_type="fasta", outfile_path=None, o
         else:
             bowtie_idx = genome_path
         data = {}
-        for sr in queries:
-            candy = []
-            try:
-                targets = find_grna(sr, bowtie_idx, cores=cores)
-            except ValueError:
-                continue
+
+        finder = GetCRISPRs(bowtie_idx, locations=locations)
+        proc_pool = multiprocessing.Pool(processes=cores, maxtasksperchild=50)
+
+        for sr, candy in proc_pool.imap_unordered(finder.find_grna, queries):
+            data[sr.id] = candy
 
             for i in range(out_num):
                 try:
-                    c_score = int(targets[i][1] * 100) / 100
+                    c_score = int(candy[i][1] * 100) / 100
                     if outfile_path is not None:
-                        print("\t".join(map(str, [sr.id, targets[i][0], c_score, targets[i][2]])), file=out_fh)
-                    candy.append((targets[i][0], c_score, targets[i][2]))
+                        print("\t".join(map(str, [sr.id, candy[i][0], c_score, candy[i][2]])), file=out_fh)
                 except IndexError:
-                    candy.append((None, None, None))
-            data[sr.id] = candy
+                    pass
 
-            print("{}: Identified {} candidate target sequences".format(sr.id, len(candy)))
+            print("{}: Identified {} candidate target sequences".format(sr.id, min(len(candy), out_num)))
 
         return data
+
     except:
         raise
+
     finally:
         if genome_type != "bowtie":
-            shutil.rmtree(bowtie_dir)
+            try:
+                shutil.rmtree(bowtie_dir)
+            except UnboundLocalError:
+                pass
+
+
+def _parse_locations(loc_path):
+    locations = {}
+    with open(loc_path, mode="rU") as loc_fh:
+        for line in loc_fh:
+            l_arr = line.strip().split()
+            try:
+                locations[l_arr[0]].append((l_arr[1], int(l_arr[2]), int(l_arr[3]), l_arr[4]))
+            except KeyError:
+                try:
+                    locations[l_arr[0]] = [(l_arr[1], int(l_arr[2]), int(l_arr[3]), l_arr[4])]
+                except IndexError:
+                    continue
+            except IndexError:
+                continue
+    return locations
 
 
 if __name__ == '__main__':
--- lib/grna_scorer.py
+++ lib/grna_scorer.py
@@ -7,104 +7,151 @@ from cutters import PAMNotFoundError
 
 from Bio.SeqUtils import GC
 
-DEFAULT_WEIGHT = lambda ons, offs, offn, a_t, p_n: 10 * (1 - ons) + offn * ((1 + offs) ** 3) + (abs(a_t) * 3) + p_n ** 2
 
+class GetCRISPRs:
 
-def find_grna(region_seq, bowtie_idx, at_optimal=0.65, pos_optimal=0.5, cutter=cas9, score_cutoff=0.01, weight=None,
-              cores=1, location=None):
-    """
-    Searches through a given sequence block to identify optimal target sequences for CRISPR RNA-directed DNA binding
+    def __init__(self, bowtie_idx, at_optimal=0.65, pos_optimal=0.5, cutter=cas9, score_cutoff=0.01, weight=None,
+                 cores=1, verbose=False, locations=None):
+        """
+        Sets the unchanging parameters for the CRISPR target search algorithm.
+        Implemented as a class to make mapping the find_grna function to a list of sequences easier
 
-    :param region_seq: str
-        Sequence string containing the target region
-    :param bowtie_idx: str
-        Path string to the bowtie sequence index
-    :param at_optimal: float
-        Optimal ratio of AT bp over total
-    :param pos_optimal: float
-        Optimal position of grna relative to the target region (0.5 is middle, 0 is 5'end, 1 is 3'end)
-    :param cutter: cutter
-        The CRISPR cutter that will be used (required for spacer length and PAM sequence)
-    :param score_cutoff: float
-        The minimum target score required to consider a gRNA/target pair as being possible to cut
-        Off-targets under this score will be ignored
-    :param weight: lambda
-        Weighted guide scoring function taking on-target score, off-target score, off-target count, 0-optimal AT ratio,
-        and 0-optimal normalized position score
-    :param cores: int
-        Number of CPU cores to use for search
-    :param location: [(str, int, int, str)]
-        List of tuple of chromosome ID, start, stop, and strand that corresponds to the query sequence location(s) in
-        the genome.If this is not set, bowtie will be used to try to obtain it.
+        Required Arguments:
 
-    :return processed_candidates: list [str, float, int]
-        List of tuples consisting of candidate target sequence (including PAM), weighted score (lower is better), and
-        total number of predicted off-target cuts. Returns list sorted by weighted score, so that the best candidate
-        is at the 0 position.
-    """
+        :param bowtie_idx: str
+            Path string to the bowtie sequence index
 
-    # Check to make sure the bowtie index exists
-    try:
-        bowtie_check_index(bowtie_idx)
-    except FileNotFoundError:
-        print("{} bowtie index not found".format(bowtie_idx))
-        raise
+        Keyword Arguments:
 
-    # Set the grna scoring algorithm to default if not passed in
-    if weight is None:
-        weight = DEFAULT_WEIGHT
+        :param at_optimal: float
+            Optimal ratio of AT bp over total
+        :param pos_optimal: float
+            Optimal position of grna relative to the target region (0.5 is middle, 0 is 5'end, 1 is 3'end)
+        :param cutter: cutter
+            The CRISPR cutter that will be used (required for spacer length and PAM sequence)
+        :param score_cutoff: float
+            The minimum target score required to consider a gRNA/target pair as being possible to cut
+            Off-targets under this score will be ignored
+        :param weight: function
+            Weighted guide scoring function taking on-target score, off-target score, off-target count,
+            0-optimal AT ratio, and 0-optimal normalized position score
+        :param cores: int
+            Number of CPU cores to use for search
+        :param locations: dict[str] = {(str, int, int, str)}
+            Dict of tuple of chromosome ID, start, stop, and strand that is keyed to the query sequence ids.
+            If this is not set, bowtie2 will be used to obtain the location in the genome of the target region.
+        """
 
-    # Find the location of the region to search in the genome
-    if location is None:
-        location = _find_location(fix_seq_object(region_seq, make_type="str"), bowtie_idx, cores=cores)
-
-        # Generate a list of possible sequences
-    candidate_target_sequences = get_candidates(region_seq, cutter=cutter)
-
-    # Align the possible sequencs to the genome for off-target mapping
-    genome_wide_hits = bowtie_get_hits(fix_seq_object_list(candidate_target_sequences.keys(), make_type="seqrecord"),
-                                       bowtie_idx, cores=cores)
-
-    processed_candidates = []
-    for candy_seq in candidate_target_sequences.keys():
-
+        # Check to make sure the bowtie index exists
         try:
-            cutter.acceptable_sequence(candy_seq)
-        except ValueError:
-            continue
+            bowtie_check_index(bowtie_idx)
+            self.idx = bowtie_idx
+        except FileNotFoundError:
+            print("{} bowtie index not found".format(bowtie_idx))
+            raise
 
-        on_score = cutter.on_target_score(cutter.remove_pam(candy_seq), candy_seq)
-        off_score = 0
-        off_count = 0
+        self.at_optimal = at_optimal
+        self.pos_optimal = pos_optimal
+        self.cutter = cutter
+        self.score_cutoff = score_cutoff
+        self.cores = cores
+        self.verbose = verbose
+        self.locations = locations
 
-        candy_start, candy_stop, candy_strand = candidate_target_sequences[candy_seq].pop()
+        if weight is None:
+            self.weight = self.default_weight
+        else:
+            self.weight = weight
 
-        for gen_seq, gen_chr, gen_pos, gen_strand, num_mismatch in genome_wide_hits[candy_seq]:
+    def find_grna(self, region_seq):
+        """
+        Searches through a given sequence block to identify optimal target sequences for CRISPR RNA-directed DNA binding
 
-            score = cutter.off_target_score(cutter.remove_pam(candy_seq), gen_seq)
+        :param region_seq: str
+            Sequence string containing the target region
 
-            if score < score_cutoff or location is None:
-                continue
+        :return processed_candidates: list [str, float, int]
+            List of tuples consisting of candidate target sequence (including PAM), weighted score (lower is better), and
+            total number of predicted off-target cuts. Returns list sorted by weighted score, so that the best candidate
+            is at the 0 position.
+        """
 
-            for r_chr, r_start, r_end, _ in location:
-                if r_chr != gen_chr or gen_pos < r_start or gen_pos > r_end:
-                    off_count += 1
-                    break
+        # Find the location of the region to search in the genome
+        current_location = None
 
-            if off_count > 0 and off_score == 0:
-                off_score = score
-            elif off_count > 0:
-                off_score = max(score * (1 + off_score), off_score * (1 + score))
-            else:
+        if self.locations is not None:
+            try:
+                current_location = self.locations[region_seq.id]
+            except KeyError:
                 pass
 
-        candy_at = 1 - GC(cutter.remove_pam(candy_seq)) / 100 - at_optimal
-        candy_pos_norm = pos_optimal - ((candy_start + candy_stop) / 2) / len(region_seq)
-        candy_weighted_score = weight(on_score, off_score, off_count, candy_at, candy_pos_norm)
-        processed_candidates.append((candy_seq, candy_weighted_score, off_count))
+        if current_location is None:
+            if self.verbose:
+                print("{}\tIdentifying Genomic Location".format(region_seq.id), end="\r")
+            current_location = _find_location(fix_seq_object(region_seq, make_type="str"), self.idx, cores=self.cores)
 
-    processed_candidates = sorted(processed_candidates, key=lambda x: x[1])
-    return processed_candidates
+        # Generate a list of possible sequences
+        candidate_target_sequences = get_candidates(region_seq, cutter=self.cutter)
+
+        # Align the possible sequencs to the genome for off-target mapping
+        if self.verbose:
+            print("{}\tLocating {} targets in genome".format(region_seq.id, len(candidate_target_sequences)), end="\r")
+        genome_wide_hits = bowtie_get_hits(
+            fix_seq_object_list(candidate_target_sequences.keys(), make_type="seqrecord"),
+            self.idx, cores=self.cores)
+
+        processed_candidates = []
+        for i, candy_seq in enumerate(candidate_target_sequences.keys()):
+
+            if self.verbose:
+                print("{}\t Scanning {} of {}".format(region_seq.id, i + 1, len(candidate_target_sequences)), end="\r")
+
+            try:
+                self.cutter.acceptable_sequence(candy_seq)
+            except ValueError:
+                continue
+
+            on_score = self.cutter.on_target_score(self.cutter.remove_pam(candy_seq), candy_seq)
+            off_score = 0
+            off_count = 0
+
+            candy_start, candy_stop, candy_strand = candidate_target_sequences[candy_seq].pop()
+
+            for gen_seq, gen_chr, gen_pos, gen_strand, num_mismatch in genome_wide_hits[candy_seq]:
+
+                score = self.cutter.off_target_score(self.cutter.remove_pam(candy_seq), gen_seq)
+
+                if score < self.score_cutoff or current_location is None:
+                    continue
+
+                for r_chr, r_start, r_end, _ in current_location:
+                    if r_chr != gen_chr or gen_pos < r_start or gen_pos > r_end:
+                        off_count += 1
+                        break
+
+                if off_count > 0 and off_score == 0:
+                    off_score = score
+                elif off_count > 0:
+                    off_score = max(score * (1 + off_score), off_score * (1 + score))
+                else:
+                    pass
+
+            candy_at = 1 - GC(self.cutter.remove_pam(candy_seq)) / 100 - self.at_optimal
+            candy_pos_norm = self.pos_optimal - ((candy_start + candy_stop) / 2) / len(region_seq)
+            candy_weighted_score = self.weight(on_score, off_score, off_count, candy_at, candy_pos_norm)
+            processed_candidates.append((candy_seq, candy_weighted_score, off_count))
+
+        processed_candidates = sorted(processed_candidates, key=lambda x: x[1])
+
+        if self.verbose:
+            print("{}\t Scanned {} Candidates\tRetained {}".format(region_seq.id,
+                                                                   len(candidate_target_sequences),
+                                                                   len(processed_candidates)))
+
+        return region_seq, processed_candidates
+
+    def default_weight(self, ons, offs, offn, a_t, p_n):
+        return 10 * (1 - ons) + offn * ((1 + offs) ** 3) + (abs(a_t) * 3) + p_n ** 2
 
 
 def get_candidates(query_seq, cutter=cas9):
@@ -127,21 +174,38 @@ def get_candidates(query_seq, cutter=cas9):
     candidates = {}
 
     def _sliding_window(s, window):
+        """
+        Generator for a sliding window
+
+        :param s: str
+            String to slide through
+        :param window: int
+            Window size
+
+        :yield: (str, int, int)
+            Window substring, start position, stop position
+        """
         for k in range(len(s) - window + 1):
             yield (s[k:k + window], k, k + window)
 
+    # Create a sliding window of the PAM motif length
     for i, target in enumerate(_sliding_window(query_seq, cutter.pam_length())):
         try:
+            # Check to see if the current window is a valid target for the cutter. Raise PAMNotFoundError if not.
             seq, k_st, k_end = target
             start, stop, strand = cutter.valid_target(seq, k_st, k_end)
-            if strand is "+":
-                candy = query_seq[start:stop].upper()
-            else:
-                candy = rc_string(query_seq[start:stop].upper())
+
+            # Keep moving if the PAM is too close to one end of the query sequence to get a full spacer
             if start < 0 or stop < 0:
                 continue
             if start > len(query_seq) or stop > len(query_seq):
                 continue
+
+            # Get the sequence of the spacer + PAM from the query sequence
+            if strand is "+":
+                candy = query_seq[start:stop].upper()
+            else:
+                candy = rc_string(query_seq[start:stop].upper())
 
             try:
                 candidates[str(candy)].append((start, stop, strand))
@@ -175,3 +239,4 @@ def _find_location(query, bowtie_idx, require_unique=False, cores=1):
                 regions = [(r_chr, r_start, r_end, r_strand)]
 
     return regions
+
